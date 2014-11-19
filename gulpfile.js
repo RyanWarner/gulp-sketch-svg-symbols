9931cf1265cd6cb8d04af8613b80c9ee4862a264
Ryan Warner <warner.ryan@gmail.com>
2014-11-19 14:51:22 -0800

    it works with an example

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp         = require( 'gulp' );
 var gutil        = require( 'gulp-util' );
 
+var connect      = require( 'gulp-connect' );
+var jade         = require( 'gulp-jade' );
+
 var sketch       = require( 'gulp-sketch' );
 var svgSymbols   = require( 'gulp-svg-symbols' );
 var rename       = require( 'gulp-rename' );
+var runSequence  = require( 'run-sequence' );
 
 
 
@@ -42,4 +46,36 @@ gulp.task( 'symbols', function(  )
     } ) )
     .pipe( rename( SVG_FILENAME ) )
     .pipe( gulp.dest(  SVG_OUTPUT_DIR ) );
+} );
+
+
+
+gulp.task( 'connect', function(  )
+{
+    connect.server(
+    {
+        root: __dirname,
+        hostname: '0.0.0.0',
+        livereload: true
+    } );
+} );
+
+gulp.task( 'jade', function( )
+{
+    return gulp.src( 'index.jade' )
+        .pipe( jade( { pretty: true } ) )
+        .on( 'error', handleError )
+        .pipe( gulp.dest( __dirname ) );
+} );
+
+
+
+gulp.task( 'default', function(  )
+{
+    runSequence(
+        [
+            'jade'
+        ],
+        'connect'
+    );   
 } );
